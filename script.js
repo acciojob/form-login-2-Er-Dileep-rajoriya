734c15de4233b6a0cce3841b23657df96406e60f
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 19:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let submitBtn = document.getElementById("submitBtn");
+let result = "";
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let fname = document.getElementById("first-name");
+  let lname = document.getElementById("last-name");
+  let phoneNum = document.getElementById("phone-num");
+  let email = document.getElementById("email-id");
+
+  //   Alert Output: First Name: John
+  //   Last Name: Doe Phone Number: 1234567890
+  //   Email ID: john.doe@example.com
+
+  result = `First Name: ${fname.value} Last Name: ${lname.value} Phone Number: ${phoneNum.value} Email ID: ${email.value}`;
+
+  alert(result);
+});
